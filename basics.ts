a7203afc59e41b7b7b462964177fff37a2715160
hassanhabibtahir <hassanhabibtahir@gmail.com>
2022-10-22 23:53:08 -0700

    added by hassan

---
+++ basics.ts
@@ -0,0 +1,25 @@
+//1,2,3,5, core
+//6 - Type Assignment & Type Inference
+function add(
+  n1: number,
+  n2: number,
+  showResult: boolean,
+  resultIs: string
+): number | undefined {
+  const _resu = n1 + n2;
+
+  if (showResult) {
+    console.log(resultIs + _resu);
+  } else {
+    return _resu;
+  }
+}
+
+const number1 = 6;
+const number2 = 5;
+const _showResult = true;
+const _resultIs = "The result is:";
+//number is required;
+add(number1, number2, _showResult, _resultIs);
+
+//7 - Object Types
